4876b88113edcaeb1990e2d17d9f310144d7473d
rohitVishwakarma <rv021561@gmail.com>
2022-06-04 17:49:58 +0530

    adding and clearing is working.

--- app.js
+++ app.js
@@ -25,8 +25,8 @@ const ItemController = (function(){
             { id : 2, name: 'Breakfast', calories: 1000}
         ],
         currentName:null,
-        totalCalories:0
-    }
+        totalCalories: 3200
+    }   
 
     return {
         retrieveData: function() {
@@ -34,7 +34,32 @@ const ItemController = (function(){
         },
         getData: function() {
             return data.items; 
+        },
+        getCalories: function() {
+            return data.totalCalories;
+        },
+        // The controller will here add the item into the data. 
+        addFoodItem: function(name, calories) {
+            let id; 
+            let curr_length = data.items.length; 
+            if(curr_length > 0) {
+                id = data.items[curr_length - 1].id + 1; // could have done just used the length but that wouldn't work.
+                // items were to be deleted . 
+            }else {
+                id = 0; 
+            }
+            newItem = new Item(id, name, parseInt(calories)); 
+            data.currentName = newItem.name;
+            data.totalCalories += newItem.calories;  
+            data.items.push(newItem); 
+            // Now the item is added.
+            return newItem;
+        },
+        clearFoodItem: function() {
+            data.items.length = 0;
+            data.totalCalories = 0;
         }
+
     }
 })();
 
@@ -50,12 +75,14 @@ const UserInterfaceController = (function(){
         itemList: '#item-list',
         addBtn: '.add-btn',
         itemNameInput: '#item-name',
-        itemCaloriesInput: '#item-calories'
+        itemCaloriesInput: '#item-calories',
+        clearBtn: '.clear-btn',
+        totalCalories: '.total-calories'
     }
 
     
     return {
-        populateItemList: function(foodItems) {
+        populateItemList: function(foodItems, totalCalories) {
             let htmlElements = '';
             foodItems.forEach(function(foodItem) {
                 htmlElements += `<li class="collection-item" id="item-${foodItem.id}">
@@ -65,6 +92,7 @@ const UserInterfaceController = (function(){
                 </a></li>`;
             });
             document.querySelector(UISelector.itemList).innerHTML = htmlElements;
+            document.querySelector(UISelector.totalCalories).innerHTML = totalCalories;
         },
         getSelectors: function() {
             return UISelector;
@@ -74,7 +102,7 @@ const UserInterfaceController = (function(){
                 name: document.querySelector(UISelector.itemNameInput).value,  
                 calories: document.querySelector(UISelector.itemCaloriesInput).value
             }
-        }
+        }, 
     }
 
 })();
@@ -92,24 +120,35 @@ const ApplicationController = (function(ItemController, UserInterfaceController)
     const loadEventListener = function() {
         const UISelectorList = UserInterfaceController.getSelectors();
         document.querySelector(UISelectorList.addBtn).addEventListener('click', addFoodItem); 
+        document.querySelector(UISelectorList.clearBtn).addEventListener('click', clearFoodItem); 
     }
+
+    function clearFoodItem(e) { 
+        ItemController.clearFoodItem();
+        UserInterfaceController.populateItemList(ItemController.getData(), ItemController.getCalories());
+        e.preventDefault(); 
+    }
+
     function addFoodItem(e) {
+        // Get Info from the form ..  
         const inputFields = UserInterfaceController.getItemInput();
         if(inputFields.name != '' && inputFields.calories != '') {
-            
-        }else {
-            alert('Enter values ');
+            const addedItem = ItemController.addFoodItem(inputFields.name, inputFields.calories);
+            UserInterfaceController.populateItemList(ItemController.getData(), ItemController.getCalories());
         }
         e.preventDefault();
     }
+
+    
     
     return {
         init: function() {
             console.log("Launching Application ...");
             const foodItems = ItemController.getData();   
+            const calories = ItemController.getCalories();
             console.log(foodItems);
             // fill the list with items 
-            UserInterfaceController.populateItemList(foodItems);
+            UserInterfaceController.populateItemList(foodItems, calories);
             loadEventListener();
         }
     }
